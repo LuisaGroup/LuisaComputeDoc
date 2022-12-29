e514f3feda724151b8b9db9664bec32ea56ffb6a
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2022-12-29 17:13:24 +0800

    add css style

--- build_html.py
+++ build_html.py
@@ -1,8 +1,77 @@
 import markdown as md
 from pathlib import Path
 import os
-programPath = Path.cwd()
-root_dir = "html"
+style_header = '''<!DOCTYPE html>
+<style>
+#carbonads {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu,
+  Cantarell, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#carbonads {
+  display: flex;
+  max-width: 330px;
+  background-color: hsl(0, 0%, 98%);
+  box-shadow: 0 1px 4px 1px hsla(0, 0%, 0%, .1);
+}
+
+#carbonads a {
+  color: inherit;
+  text-decoration: none;
+}
+
+#carbonads a:hover {
+  color: inherit;
+}
+
+#carbonads span {
+  position: relative;
+  display: block;
+  overflow: hidden;
+}
+
+#carbonads .carbon-wrap {
+  display: flex;
+}
+
+.carbon-img {
+  display: block;
+  margin: 0;
+  line-height: 1;
+}
+
+.carbon-img img {
+  display: block;
+}
+
+.carbon-text {
+  font-size: 13px;
+  padding: 10px;
+  line-height: 1.5;
+  text-align: left;
+}
+
+.carbon-poweredby {
+  display: block;
+  padding: 8px 10px;
+  background: repeating-linear-gradient(-45deg, transparent, transparent 5px, hsla(0, 0%, 0%, .025) 5px, hsla(0, 0%, 0%, .025) 10px) hsla(203, 11%, 95%, .4);
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: .5px;
+  font-weight: 600;
+  font-size: 9px;
+  line-height: 1;
+}
+img {
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 5px;
+  width: 1500px;
+}
+</style>
+'''
+programPath = str(Path.cwd())
+root_dirs = ["en-us", "zh-cn"]
 
 
 def makedir(path: str):
@@ -10,7 +79,7 @@ def makedir(path: str):
         os.mkdir(path)
 
 
-def html_path(md_path: str):
+def html_path(md_path: str, new_dir: str):
     new_path = ""
     for i in md_path:
         if i == '\\':
@@ -18,7 +87,7 @@ def html_path(md_path: str):
         else:
             new_path += i
     elements = new_path.split('/')
-    folder = root_dir + '/'
+    folder = new_dir + '/'
     if len(elements) <= 1:
         return folder + new_path.replace('.md', '.html')
     for i in range(len(elements) - 1):
@@ -28,15 +97,25 @@ def html_path(md_path: str):
     return folder
 
 
-if __name__ == '__main__':
-    makedir(root_dir)
-    for path in Path('.').rglob('*.md'):
+def process_path(root_dir: str):
+    if not os.path.exists(root_dir):
+        return
+    html_rootpath = root_dir + "-html"
+    makedir(html_rootpath)
+    local_path = programPath + '/' + root_dir
+    for path in Path(root_dir).rglob('*.md'):
         pt = path.absolute()
-        new_path = html_path(str(pt.relative_to(programPath)))
+        new_path = html_path(str(pt.relative_to(local_path)), html_rootpath)
         f = open(pt, "r", encoding="utf-8")
         s = f.read()
         f.close()
         html = md.markdown(s)
         f = open(new_path, "w", encoding="utf-8")
+        f.write(style_header)
         f.write(html)
         f.close()
+
+
+if __name__ == '__main__':
+    for i in root_dirs:
+        process_path(i)
